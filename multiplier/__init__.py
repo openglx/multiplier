89917e75b47c40668c26accd56fc989872c785e7
openglx <openglx@users.noreply.github.com>
2025-05-12 23:51:19 +0100

    story mode

--- multiplier/__init__.py
+++ multiplier/__init__.py
@@ -6,6 +6,7 @@ import random
 
 random.seed()
 
+
 class OneResult:
     first: int
     second: int
@@ -22,17 +23,20 @@ class OneResult:
         self.received = None
         self.score = None
 
+
 @dataclass
 class Session:
     results: list[OneResult] = field(default_factory=list)
+    story: list[str] = field(default_factory=list)
+    question_time: bool = False
 
-def add_result(sessions: dict, uuid: UUID, iteration: int, answer: int, received: datetime):
-    if not uuid in sessions:
-        raise IndexError("We seem to have lost your session...")
-    session = sessions[uuid]
+
+def add_result(session: Session, iteration: int, answer: int, received: datetime):
     expected_iteration = len(session.results) - 1
     if expected_iteration != iteration:
-        raise ValueError(f"Iteration mismatch, wanted {expected_iteration} got {iteration}, multiple tabs open?")
+        raise ValueError(
+            f"Iteration mismatch, wanted {expected_iteration} got {iteration}, multiple tabs open?"
+        )
     result = session.results[iteration]
     result.answer = answer
     result.received = received
@@ -40,36 +44,91 @@ def add_result(sessions: dict, uuid: UUID, iteration: int, answer: int, received
     score += ", too slow" if received - result.sent > timedelta(seconds=6) else ""
     result.score = score
 
+
+def space_explorer():
+    return [
+        # 0
+        """You are commanding a spacecraft and must input the
+        coordinates for next sectors to explore. The coordinates
+        are always the result of the multiplication of two factors.
+        Enter carefully and you will reach your destination.
+        """,
+        # 1
+        """You have reached a new sector - there is a star system nearby.
+        Navigate towards it.""",
+        # 2
+        """As you approach the gas giants at the edge of the star system,
+        avoid their gravity well.
+        """,
+        # 3
+        """You are past the gas giants, avoid the asteroid belt.""",
+        # 4
+        """Scan the inner planets for any life signs.""",
+        # 5
+        """A faint signal was detected, reach closer.""",
+        # 6
+        """You are listening to the echo of your own radar, nothing else
+        to see here. Move on to next star system.""",
+        # 7
+        """Continue searching...""",
+        # 8
+        """Navigate to this nebula to recharge your dilithium reserves.""",
+        # 9
+        """Resume your search!""",
+    ]
+
+
 sessions = {}
+
 
 def create_app(test_config=None):
     # create and configure the app
     app = Flask(__name__, instance_relative_config=True)
 
-    @app.route('/', methods=('GET', 'POST'))
+    @app.route("/", methods=("GET", "POST"))
     def hello():
-        if request.method == 'POST':
-            uuid = request.form['uuid']
-            iteration = int(request.form['iteration'])
-            answer = int(request.form['answer'])
-            received = datetime.now()
-            add_result(sessions, uuid, iteration, answer, received)
+        if request.method == "POST":
+            uuid = request.form["uuid"]
+            session = sessions[uuid]
+            session.question_time = not session.question_time
+
+            if story := request.form.get("story"):
+                if not session.story:
+                    if story == "space_explorer":
+                        session.story = space_explorer()
+                    else:
+                        raise ValueError("I don't know this story...")
+            else:
+                iteration = int(request.form["iteration"])
+                answer = int(request.form["answer"])
+                received = datetime.now()
+                add_result(session, iteration, answer, received)
         else:
             uuid = str(uuid4())
             sessions[uuid] = Session()
+            session = sessions[uuid]
 
-        if len(sessions[uuid].results) > 9:
-            return render_template("results.html", session=sessions[uuid])
+        if len(session.results) > 9:
+            return render_template("results.html", session=session)
 
-        result = OneResult()
-        sessions[uuid].results.append(result)
-        return render_template(
-            "numbers.html",
-            uuid=uuid,
-            iteration=len(sessions[uuid].results) - 1,
-            first=result.first,
-            second=result.second,
-        )
+        if not session.story:
+            return render_template("pick_story.html", uuid=uuid)
+
+        if session.question_time:
+            result = OneResult()
+            session.results.append(result)
+            return render_template(
+                "numbers.html",
+                uuid=uuid,
+                iteration=len(session.results) - 1,
+                first=result.first,
+                second=result.second,
+            )
+        else:
+            return render_template(
+                "story.html",
+                uuid=uuid,
+                message=session.story[len(session.results)],
+            )
 
     return app
-
